c6f395aeecc4659c3cb580874b0a5480af3aeca6
Bryan Duque <138838133+BryanDuque3@users.noreply.github.com>
2024-11-07 21:13:52 -0500

    Delete app directory

--- app/models/line_item.rb
+++
@@ -1,9 +0,0 @@
-class LineItem < ApplicationRecord
-  belongs_to :product
-  belongs_to :cart
-
-  # Define total_price method within the class
-  def total_price
-    product.price * quantity
-  end
-end
